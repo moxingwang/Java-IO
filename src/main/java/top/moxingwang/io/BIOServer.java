341612f7afd87c2c906954e6d09076ac8110e9b1
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-06-19 09:40:19 +0800

    commit

---
+++ src/main/java/top/moxingwang/io/BIOServer.java
@@ -0,0 +1,35 @@
+package top.moxingwang.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @description:
+ * @author: MoXingwang 2019-06-19 09:36
+ **/
+public class BIOServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+
+                int len;
+                byte[] data = new byte[1024];
+                InputStream inputStream = socket.getInputStream();
+                while ((len = inputStream.read(data)) != -1) {
+                    System.out.println(new String(data, 0, len));
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
--- src/main/java/top/moxingwang/io/IOClient.java
+++ src/main/java/top/moxingwang/io/IOClient.java
@@ -1,6 +1,7 @@
 package top.moxingwang.io;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 import java.util.Date;
 
@@ -11,13 +12,14 @@ import java.util.Date;
 public class IOClient {
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("127.0.0.1", 3333);
-            while (true) {
-                try {
-                    socket.getOutputStream().write((new Date() + ": hello world").getBytes());
-                    Thread.sleep(2000);
-                } catch (Exception e) {
-                }
+            Socket socket = new Socket("127.0.0.1", 8080);
+            socket.getOutputStream().write((new Date() + ": hello world").getBytes());
+
+            int len;
+            byte[] data = new byte[1024];
+            InputStream inputStream = socket.getInputStream();
+            while ((len = inputStream.read(data)) != -1) {
+                System.out.println(new String(data, 0, len));
             }
         } catch (IOException e) {
             e.printStackTrace();
